b18edeedb5c211e137fc8ddd28804ce600af7285
Abdul Hameed Sabree <ahs4448@yahoo.com>
2020-04-12 19:49:20 -0400

    upgrade

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ClickyApp from './components/board/board'
+class App extends Component {
+  render() {
+    return (
+    
+      <ClickyApp/>
+    );
+  }
+}
+
+export default App;

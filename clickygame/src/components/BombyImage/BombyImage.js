0a2b2a4dfcfadb024dbfd3e6e772d659c5b34217
Abdul Hameed Sabree <ahs4448@yahoo.com>
2020-04-12 18:19:58 -0400

    new folder visible

---
+++ clickygame/src/components/BombyImage/BombyImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function BombyImage (){
+    return(
+        
+    )
+}
+
+export default BombyImage
---
+++ clickygame/src/components/board/ScoreNav/ScoreNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ScoreNav (){
+  return(
+    <nav className="navbar navbar-light bg-light">
+    <a className="navbar-brand" href="#">Bomberman Clicky Game </a> <h1>Score:0 | Top Score:0</h1>
+  </nav>
+  )
+}
+
+export default ScoreNav

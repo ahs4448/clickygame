58a37dc1e129d36385e33606ac117ce30d96b1f9
Abdul Hameed Sabree <ahs4448@yahoo.com>
2020-04-12 18:13:08 -0400

    folders showing

---
+++ clickygame/build/precache-manifest.4f3751bc4fc180e48c48ae727ec49eaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8608610ced2e9bbeb92",
+    "url": "/ClickyGame/static/css/main.3b26e13c.chunk.css"
+  },
+  {
+    "revision": "e8608610ced2e9bbeb92",
+    "url": "/ClickyGame/static/js/main.e8608610.chunk.js"
+  },
+  {
+    "revision": "245a4c78fe8929132dc2",
+    "url": "/ClickyGame/static/js/1.245a4c78.chunk.js"
+  },
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "86da67d87449b6e344babf20925b4871",
+    "url": "/ClickyGame/index.html"
+  }
+];

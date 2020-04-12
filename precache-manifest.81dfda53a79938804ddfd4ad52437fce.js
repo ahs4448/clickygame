d3defd1f07a813581e7760919ccd3d92c5411561
Abdul Hameed Sabree <ahs4448@yahoo.com>
2020-04-12 17:20:27 -0400

    Updates

---
+++ precache-manifest.81dfda53a79938804ddfd4ad52437fce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d36aa0fa1e2f7b8d72201ac9077cbac",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "05ca35baa22e38341de7",
+    "url": "/clickygame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e1f15becc05f80682a28",
+    "url": "/clickygame/static/js/2.8aa4e476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.8aa4e476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ca35baa22e38341de7",
+    "url": "/clickygame/static/js/main.8f11ba4e.chunk.js"
+  },
+  {
+    "revision": "f5211b3e66d21586d164",
+    "url": "/clickygame/static/js/runtime-main.cacafc75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickygame/static/media/logo.5d5d9eef.svg"
+  }
+]);
